3d1b1870687b4f62dd801884659e9262f60609e7
Adel1250 <adelashrafejada@gmail.com>
2023-08-21 15:47:43 +0200

    First Upload

---
+++ product-service/src/main/java/com/adel/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.adel.productservice.repository;
+
+import com.adel.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
