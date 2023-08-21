6a46c4fa321267fb75dcfeb8eb84fd7c2298d5ca
Adel1250 <adelashrafejada@gmail.com>
2023-08-21 17:55:49 +0200

    Initiate order service

---
+++ order-service/src/main/java/com/adel/orderservice/model/OrderLineItems.java
@@ -0,0 +1,9 @@
+package com.adel.orderservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "t_order_line_")
+public class OrderLineItems {
+}
